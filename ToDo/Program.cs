29cdca8523109795f83de5f231111c9cdaeb3a25
Joaquina Girbau <girbaujoaquina2@gmail.com>
2025-06-03 12:54:31 -0300

    agrego tareas en pendientes

--- ToDo/Program.cs
+++ ToDo/Program.cs
@@ -1,20 +1,12 @@
-﻿
-namespace EspacioTareas
-{
-    public class Tarea
-    {
-        public int TareaID { get; set; }
-        public string Descripcion { get; set; }
-        public int Duracion { get; set; }
+﻿using EspacioTareas;
+List<Tarea> tareasPendientes = new List<Tarea>();
+List<Tarea> tareasRealizadas = new List<Tarea>();
 
-        public Tarea(int TareaID, string Descripcion, int Duracion)
-        {
-            this.TareaID = TareaID;
-            this.Descripcion = Descripcion;
-            this.Duracion = Duracion;
-        }
-    }
-}
+Tarea Tarea1 = new Tarea(0, "Bañar a pipo", 60);
+Tarea Tarea2 = new Tarea(0, "Realizar Algoritmos", 120);
+Tarea Tarea3 = new Tarea(0, "Resumir teoria mat discreta", 80);
 
-
+tareasPendientes.Add(Tarea1);
+tareasPendientes.Add(Tarea2);
+tareasPendientes.Add(Tarea3);
 
---
+++ ToDo/ToDo.cs
@@ -0,0 +1,16 @@
+namespace EspacioTareas
+{
+    public class Tarea
+    {
+        public int TareaID { get; set; }
+        public string Descripcion { get; set; }
+        public int Duracion { get; set; }
+
+        public Tarea(int TareaID, string Descripcion, int Duracion)
+        {
+            this.TareaID = TareaID;
+            this.Descripcion = Descripcion;
+            this.Duracion = Duracion;
+        }
+    }
+}
